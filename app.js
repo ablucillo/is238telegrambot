47629e481e0585a006e3b6aeac7bd0946f1b6ed7
unknown <edsangel@up.edu.ph>
2021-12-29 19:27:42 +0800

    CHORE: Added the email functionality and fixed the bug on REPORTS keyword. Additional updates were for code refactoring and folder restructuring.

--- app.js
+++ app.js
@@ -2,124 +2,139 @@ require("dotenv").config();
 
 const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
-const quotes = require("./quotes.json");
+
+const PRESIDENTIABLES = require('./data/presidentiables.json');
+const QUOTES = require("./data/quotes.json");
+const RESERVED_KEYWORDS = require('./data/reserved-keywords.json');
+
+const validateEmail = require('./utils/email-validator');
+const { mailer, generateMessage } = require('./utils/mailer');
 
 const SELECT_CANDIDATE = "Select candidate";
 const CONFIRM_REPORT = "Are these details correct?";
 
-bot.onText(/QUOTES/, function (msg, match) {
-  bot.sendMessage(msg.chat.id, SELECT_CANDIDATE, {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: "Emmanuel 'Manny' Pacquiao",
-            callback_data: "manny",
-          },
-        ],
-        [
-          {
-            text: "Ferdinand 'Bongbong' Marcos",
-            callback_data: "bongbong",
-          },
-        ],
-        [
-          {
-            text: "Francisco 'Isko' Moreno",
-            callback_data: "isko",
-          },
-        ],
-        [
-          {
-            text: "Ma. Leonor 'Leni' Robredo",
-            callback_data: "leni",
-          },
-        ],
-        [
-          {
-            text: "Panfilo 'Ping' Lacson",
-            callback_data: "ping",
-          },
-        ],
-      ],
-    },
-  });
+//TODO: First, need to check if user is logged in
+//TODO: Then, these variables need to be associated with a specific logged in user
+/**
+ * This function generates and sends the report (if user opted to).
+ *
+ * @param chatId 
+ * @param msg 
+ */
+const reportGenerator = (chatId, msg) => {
+	if (!reportDetails.hasOwnProperty('name')) {
+		reportDetails['name'] = msg.text;
+		bot.sendMessage(chatId, "What is your email?");
+	} else if (reportDetails.hasOwnProperty('name') && !reportDetails.hasOwnProperty('email')) {
+		if (validateEmail(msg.text)) {
+			reportDetails['email'] = msg.text;
+			bot.sendMessage(chatId, "What is your report?");
+		} else {
+			bot.sendMessage(chatId, "Please provide a valid email.");
+		}
+	} else if (reportDetails.hasOwnProperty('name') && reportDetails.hasOwnProperty('email') &&
+		!reportDetails.hasOwnProperty('report')) {
+		reportDetails['report'] = msg.text;
+
+		bot.sendMessage(chatId,
+			'You requested a report with the following details\n' +
+			`Name: ${reportDetails.name}\n` +
+			`Email: ${reportDetails.email}\n` +
+			`Report: ${reportDetails.report}\n\n` +
+			`${CONFIRM_REPORT}`, {
+			reply_markup: {
+				inline_keyboard: [
+					[
+						{
+							text: "Yes",
+							callback_data: "report_yes",
+						}
+					],
+					[
+						{
+							text: "No",
+							callback_data: "report_no",
+						}
+					]
+				]
+			}
+		}
+		);
+	}
+};
+
+bot.onText(/QUOTES/, (msg) => {
+	bot.sendMessage(msg.chat.id, SELECT_CANDIDATE, {
+		reply_markup: {
+			inline_keyboard: [...PRESIDENTIABLES]
+		}
+	});
 });
 
+bot.onText(/REPORT/, (msg) => {
+	if (!reportStarted) {
+		reportDetails = {};
+		reportStarted = true;
+		bot.sendMessage(msg.chat.id, "What is your name?");
+	}
+});
+
+bot.on("message", (msg) => {
+	const chatId = msg.chat.id;
+
+	// Restrict reserved keywords
+	if (reportStarted && RESERVED_KEYWORDS.includes(msg.text)) {
+		bot.sendMessage(chatId, "The input provided is reserved. Please try again.");
+		return;
+	}
+	
+	if (reportStarted) {
+		reportGenerator(chatId, msg);
+	}
+});
+
+let reportDetails = {};
+let reportSent = false;
+let reportStarted = false;
 bot.on("callback_query", function onCallbackQuery(callbackQuery) {
 	const data = callbackQuery.data;
 	const msg = callbackQuery.message;
 
-	if (msg.text === SELECT_CANDIDATE) {
-		const parsed_quotes = JSON.parse(JSON.stringify(quotes.candidates));
-		let candidate = parsed_quotes.filter((c) => c.candidate === data)[0];
-		let quote =
-		  candidate.quotes[Math.floor(Math.random() * candidate.quotes.length)];
+	// For the presidentible quotes
+	if (data.startsWith("quote_")) {
+		const candidate = QUOTES.find((c) => c.id === data);
+		const quote = candidate.quotes[Math.floor(Math.random() * candidate.quotes.length)];
 
 		bot.sendMessage(msg.chat.id, quote);
-	} else if (msg.text === CONFIRM_REPORT) {
-		if (data == "yes") {
-			bot.sendMessage(msg.chat.id, "Sending email");
-			//TODO: Need to send email
+
+	// For the report email notification
+	} else if (data === 'report_yes') {
+		bot.sendMessage(msg.chat.id, "Sending email. Please wait.");
+		reportStarted = false;
+		reportSent = true;
+
+		// Send the email
+		mailer.sendMail(generateMessage(reportDetails), (err, info) => {
+			if (err) {
+				console.log(err);
+				bot.sendMessage(msg.chat.id, "There was a problem sending the report to your email. Please try again later.");
+			} else {
+				bot.sendMessage(msg.chat.id, "Email sent!");
+			}
+		});
+
+	} else if (data === 'report_no') {
+		if (reportSent) {
+			bot.sendMessage(msg.chat.id, "Sorry, the report was already sent.");
+			reportStarted = false;
 		} else {
-			bot.sendMessage(msg.chat.id, "Try again");
+			bot.sendMessage(msg.chat.id, "Try again.");
+			reportSent = false;
+			reportDetails = {};
+
+			// Ask the user again
+			bot.sendMessage(msg.chat.id, "What is your name?");
+			reportStarted = true;
 		}
 	}
-});
-
-
-//TODO: First, need to check if user is logged in
-//TODO: Then, these variables need to be associated with a specific logged in user
-const wait = []; 
-const report_resp = [];
-
-bot.onText(/REPORT/, (msg) => {
-	const chatId = msg.chat.id;
-	bot.sendMessage(chatId, "What is your name?");
-	wait.push("name");
-});
-
-//Wait for any message, get details
-bot.on('message', (msg) => {
-	const chatId = msg.chat.id;
-	if (wait.includes("name")) {
-		report_resp.push(msg.text);
-		wait.splice(wait.indexOf("name"), 1);
-
-		wait.push("email");
-		bot.sendMessage(chatId, "What is your email?");
-	} else if (wait.includes("email")) {
-		report_resp.push(msg.text);
-		//TODO: Need to validate email if correct format
-		wait.splice(wait.indexOf("email"), 1);
-		
-		wait.push("report");
-		bot.sendMessage(chatId, "What is your report?");
-	} else if (wait.includes("report")) {
-		report_resp.push(msg.text);
-		wait.splice(wait.indexOf("report"), 1);
-
-		bot.sendMessage(chatId, "Your name is " + report_resp.slice(0,1)
-			+ "\nYour email is " + report_resp.slice(1,2)
-			+ "\nYour report is " + report_resp.slice(2,3));
-	
-		bot.sendMessage(chatId, CONFIRM_REPORT, {
-			reply_markup: {
-			  inline_keyboard: [
-				[
-				  {
-					text: "Yes",
-					callback_data: "yes",
-				  },
-				],
-				[
-				  {
-					text: "No",
-					callback_data: "no",
-				  },
-				],
-			  ],
-			},
-		});
-	}	  
 });
